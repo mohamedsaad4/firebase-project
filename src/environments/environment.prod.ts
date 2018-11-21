58f76ef4baf00cfe0e9fb97a9e03f85eab50549c
mohamedsaad4 <mahameds19999@gmail.com>
2018-11-21 02:21:07 +0200

    deploying the project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCMTTuhDDj2HTEmuax41ghEeZD8GTmMOOo",
+    authDomain: "fir-4c0ca.firebaseapp.com",
+    databaseURL: "https://fir-4c0ca.firebaseio.com",
+    projectId: "fir-4c0ca",
+    storageBucket: "fir-4c0ca.appspot.com",
+    messagingSenderId: "573351203672"
+  }
 };
